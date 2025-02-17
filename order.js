2d447fe082865cf76ee244c4535e1aef091ac858
Mikey33333333 <maom95906@gmail.com>
2025-02-17 22:29:46 +0700

    Add files via upload

---
+++ order.js
@@ -0,0 +1,20 @@
+const orders = [
+   {
+      productName: 'Javascript Tutorial',
+      productNumber: '84943',
+      productStatus: 'Due',
+      status: 'Pending',
+   },
+   {
+      productName: 'Css Full Course',
+      productNumber: '40040',
+      productStatus: 'Mikey',
+      status: 'Declined',
+   },
+   {
+      productName: 'Flex-Box Tutorial',
+      productNumber: '40594',
+      productStatus: 'Paid',
+      status: 'Active',
+   }
+]
